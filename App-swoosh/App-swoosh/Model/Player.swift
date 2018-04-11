2c0e192cd73ff7cae3e660edc6ff243fd9758e97
Francisco Ramirez <new-user@francisoramirez>
2018-04-10 19:16:28 -0700

    passing data

---
+++ App-swoosh/App-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  App-swoosh
+//
+//  Created by NEW-USER on 4/10/18.
+//  Copyright © 2018 Francisco. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
